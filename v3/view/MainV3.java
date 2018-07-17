897ee1d0a21c38c2e2709cf9f1ea45fa55bc8f2c
teteretu <teteretu@yahoo.com>
2018-07-17 00:16:37 -0300

    Implementação terceira versão

--- v3/view/MainV3.java
+++ v3/view/MainV3.java
@@ -12,6 +12,11 @@ import java.util.List;
 
 public class MainV3 {
 	
+	private static long four;
+	private static long differents;
+	private static long sequences;
+	private static long datePassed;
+	
 	enum Card {
 	    T(10),J(11),Q(12),K(13),A(14);
 	 
@@ -26,44 +31,30 @@ public class MainV3 {
 	}
 
 	public static void main(String[] args) {
-//		Descomentar essa parte na hora de gerar versão
-//		if (args.length < 3) {
-//			System.out.println("Informe 3 arquivos para serem lidos.");
-//			System.exit(1);
-//		}
+		if (args.length < 3) {
+			System.out.println("Informe 3 arquivos para serem lidos.");
+			System.exit(1);
+		}
 		
-		// Tudo isso aqui é pra ser removido na hora de gerar versão,
-		// ta aqui só pra debug.
-		args = new String[3];
-		args[0] = "teste.txt";
-		args[1] = "teste.txt";
-		args[2] = "teste.txt";
-		// Até aqui
-		
-		long four = 0;
-		long nothing = 0;
-		long differents = 0;
-		long sequences = 0;
-		long datePassed = 0;
-		long dateBeggin = 0;
 		String line = new String();
-
-		// Início do corpo do método readFileAndCalculate
-		BufferedReader br = null;
-		FileReader fr = null;
-		List<String> hands = new ArrayList<>();
 
 		// Esse for é pra executar pros 3 primeiros argumentos
 		for (int i = 0; i < 3; i++) {
+			
+			long dateBeggin = System.currentTimeMillis();
+			
 			four = 0;
-			nothing = 0;
 			differents = 0;
 			sequences = 0;
 			datePassed = 0;
-			
-			dateBeggin = System.currentTimeMillis();
-			
+
+			// Início do corpo do método readFileAndCalculate
+			BufferedReader br = null;
+			FileReader fr = null;
+			List<String> hands = new ArrayList<>();
+
 			try {
+				
 				//br = new BufferedReader(new FileReader(FILENAME));
 				fr = new FileReader(args[i]);
 				br = new BufferedReader(fr);
@@ -75,88 +66,87 @@ public class MainV3 {
 				boolean sequence = false;
 				
 				while ((sCurrentLine = br.readLine()) != null) {
-					
-					
 					hands.add(sCurrentLine);
 					
 					// Com arquivos com menos de 5000 linhas, não ta entrando, CONFIRMAR EM CORREÇÃO
 					if (thousand > 5000) {
 						if (hands != null && hands.size() > 0) {
-//							System.out.println("calculando");
-
 							// Início do corpo do método verifyHands
 							for (String hand : hands) {
-								String[] Hands = hand.split(("\\s+"));
-								
+
+								String[] tmpHand = hand.split(("\\s+"));
 								// Início do corpo do método isFourEquals
 								hand = hand.replaceAll("\\s+","");
 								String tmp = new String(hand);
 								short j = 0;
-								
-								// Não entendi o que acontece aqui, ele substitui hand com o conteúdo de tmp,
-								// que é uma linha, menos 1 caractere, ou seja, é a linha com 4 "cartas".
-								// Essa rotina foi retirada do método isFourEquals.
 								while(j < tmp.length()-1) {
 									hand = hand.replace(tmp.charAt(j++) + "", "");
 									
 									if (hand.length() > 3)
 										hand = tmp;
-									else if(hand.length() > 1) //se encontrou pelo menos um par de cartas
+									else if(hand.length() > 1) { //se encontrou pelo menos um par de cartas
 										result = "nada";
-									else
-										result = "four";	
+										break;
+									} else {
+										result = "four";
+										break;
+									}
 								}
 								// Fim do corpo do método isFourEquals
 								
-								// Início do corpo do método isSequency
-								int[] handsNumber = new int[5];
-								
-								j = 0;
-								//transforma string em aray de números
-								while (j < Hands.length) {
-									if(Hands[j].equalsIgnoreCase("T")) {
-										handsNumber[j] = Card.T.getCard();
-									} else if (Hands[j].equalsIgnoreCase("J")) {
-										handsNumber[j] = Card.J.getCard();
-									} else if (Hands[j].equalsIgnoreCase("Q")) {
-										handsNumber[j] = Card.Q.getCard();
-									} else if (Hands[j].equalsIgnoreCase("K")) {
-										handsNumber[j] = Card.K.getCard();
-									} else if (Hands[j].equalsIgnoreCase("A")) {
-										handsNumber[j] = Card.A.getCard();
-									} else {
-										handsNumber[j] = Integer.parseInt(Hands[j]);
-									}
-									j++;
-								}
-								//ordena o array
-								Arrays.sort(handsNumber);
-								
-								j = 0;
-								short isSequence = 0;
-								//conta os que estão em sequencia
-								while(j < (handsNumber.length - 1)) {
-									
-									if ((handsNumber[j] + 1) == handsNumber[j+1])
-										isSequence++;
-									j++;
-								}
-								//quer dizer que são sequencia
-								if (isSequence > 3)
-									sequence = true;
-								else
-									sequence = false;
-								// Fim do corpo do método isSequency
-								
-								//se existem quatro cartas iguais
 								if(result.equalsIgnoreCase("four")) {
 									four++;
-								} else if (result.equalsIgnoreCase("nada")) { //se existe pares
-									nothing++;
-								} else if (sequence == true) { //sequência
-									sequences++;
-								}else {		//todos diferentes
-									differents++;
+									result = "";
+								} else if (result.equalsIgnoreCase("nada")) { //se existe pares encerra a verificação
+									result = "";
+								} else {
+									
+									// Início do corpo do método isSequency
+									int[] handsNumber = new int[5];
+									
+									j = 0;
+									//transforma string em aray de números
+									while (j < tmpHand.length) {
+										if(tmpHand[j].equalsIgnoreCase("T")) {
+											handsNumber[j] = Card.T.getCard();
+										} else if (tmpHand[j].equalsIgnoreCase("J")) {
+											handsNumber[j] = Card.J.getCard();
+										} else if (tmpHand[j].equalsIgnoreCase("Q")) {
+											handsNumber[j] = Card.Q.getCard();
+										} else if (tmpHand[j].equalsIgnoreCase("K")) {
+											handsNumber[j] = Card.K.getCard();
+										} else if (tmpHand[j].equalsIgnoreCase("A")) {
+											handsNumber[j] = Card.A.getCard();
+										} else {
+											handsNumber[j] = Integer.parseInt(tmpHand[j]);
+										}
+										j++;
+									}
+									//ordena o array
+									Arrays.sort(handsNumber);
+									
+									j = 0;
+									short isSequence = 0;
+									//conta os que estão em sequencia
+									while(j < (handsNumber.length - 1)) {
+										
+										if ((handsNumber[j] + 1) == handsNumber[j+1])
+											isSequence++;
+										j++;
+									}
+									//quer dizer que são sequencia
+									if (isSequence > 3)
+										sequence = true;
+									else
+										sequence = false;
+									// Fim do corpo do método isSequency
+									
+									if (sequence) { //sequência
+										sequences++;
+										sequence = false;
+									}else {		//todos diferentes
+										differents++;
+									}
 								}
 							}
 							// Fim do corpo do método verifyHands
@@ -173,16 +163,80 @@ public class MainV3 {
 				if (hands != null && hands.size() > 0) {
 					// Início do corpo do método verifyHands
 					for (String hand : hands) {
+
+						String[] tmpHand = hand.split(("\\s+"));
+						// Início do corpo do método isFourEquals
+						hand = hand.replaceAll("\\s+","");
+						String tmp = new String(hand);
+						short j = 0;
+						while(j < tmp.length()-1) {
+							hand = hand.replace(tmp.charAt(j++) + "", "");
+							
+							if (hand.length() > 3)
+								hand = tmp;
+							else if(hand.length() > 1) { //se encontrou pelo menos um par de cartas
+								result = "nada";
+								break;
+							} else {
+								result = "four";
+								break;
+							}
+						}
+						// Fim do corpo do método isFourEquals
 						
-						//se existem quatro cartas iguais
 						if(result.equalsIgnoreCase("four")) {
+							result = "";
 							four++;
-						} else if (result.equalsIgnoreCase("nada")) { //se existe pares
-							nothing++;
-						} else if (sequence == true) { //sequência
-							sequences++;
-						}else {		//todos diferentes
-							differents++;
+						} else if (result.equalsIgnoreCase("nada")) { //se existe pares encerra a verificação
+							result = "";
+						} else {
+							
+							// Início do corpo do método isSequency
+							int[] handsNumber = new int[5];
+							
+							j = 0;
+							//transforma string em aray de números
+							while (j < tmpHand.length) {
+								if(tmpHand[j].equalsIgnoreCase("T")) {
+									handsNumber[j] = Card.T.getCard();
+								} else if (tmpHand[j].equalsIgnoreCase("J")) {
+									handsNumber[j] = Card.J.getCard();
+								} else if (tmpHand[j].equalsIgnoreCase("Q")) {
+									handsNumber[j] = Card.Q.getCard();
+								} else if (tmpHand[j].equalsIgnoreCase("K")) {
+									handsNumber[j] = Card.K.getCard();
+								} else if (tmpHand[j].equalsIgnoreCase("A")) {
+									handsNumber[j] = Card.A.getCard();
+								} else {
+									handsNumber[j] = Integer.parseInt(tmpHand[j]);
+								}
+								j++;
+							}
+							//ordena o array
+							Arrays.sort(handsNumber);
+							
+							j = 0;
+							short isSequence = 0;
+							//conta os que estão em sequencia
+							while(j < (handsNumber.length - 1)) {
+								
+								if ((handsNumber[j] + 1) == handsNumber[j+1])
+									isSequence++;
+								j++;
+							}
+							//quer dizer que são sequencia
+							if (isSequence > 3)
+								sequence = true;
+							else
+								sequence = false;
+							// Fim do corpo do método isSequency
+							
+							if (sequence) { //sequência
+								sequences++;
+								sequence = false;
+							}else {		//todos diferentes
+								differents++;
+							}
 						}
 					}
 					// Fim do corpo do método verifyHands
@@ -213,9 +267,11 @@ public class MainV3 {
 		
 			line += String.valueOf(datePassed) + " " + String.valueOf(four) +
 				" " + String.valueOf(differents) + " " + String.valueOf(sequences) + "\n";
+
 		}
 		
 		try {
+			// Início do corpo do método writeFile
 			File file = new File("saida.txt");
 			
 			if (!file.exists()) {
