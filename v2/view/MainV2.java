976154f5e003bd6278ec299272596989ca26e830
teteretu <teteretu@yahoo.com>
2018-07-16 21:30:28 -0300

    Jar da v2 gerado

--- v2/view/MainV2.java
+++ v2/view/MainV2.java
@@ -5,26 +5,19 @@ import control.Archive;
 public class MainV2 {
 
 	public static void main(String[] args) {
-//		if (args.length < 3) {
-//			System.out.println("Informe 3 arquivos para serem lidos.");
-//			System.exit(1);
-//		}
+		if (args.length < 3) {
+			System.out.println("Informe 3 arquivos para serem lidos.");
+			System.exit(1);
+		}
 		
 		String line = new String();
 		
-//		for (int x = 0; x < 3; x++) {
-//			Archive.readFileAndCalculate(args[x]);
-//			line += String.valueOf(Archive.getDatePassed()) + " " + String.valueOf(Archive.getFour()) +
-//					" " + String.valueOf(Archive.getDifferents()) + " " + String.valueOf(Archive.getSequences()) + "\n";
-//		}
-		Archive.readFileAndCalculate("poker2K.txt");
-		line += String.valueOf(Archive.getDatePassed()) + " " + String.valueOf(Archive.getFour()) +
-				" " + String.valueOf(Archive.getDifferents()) + " " + String.valueOf(Archive.getSequences()) + "\n";
-
-		Archive.readFileAndCalculate("poker2K.txt");
-		line += String.valueOf(Archive.getDatePassed()) + " " + String.valueOf(Archive.getFour()) +
-				" " + String.valueOf(Archive.getDifferents()) + " " + String.valueOf(Archive.getSequences()) + "\n";
-		System.out.println("write in archive");
+		for (int x = 0; x < 3; x++) {
+			Archive.readFileAndCalculate(args[x]);
+			line += String.valueOf(Archive.getDatePassed()) + " " + String.valueOf(Archive.getFour()) +
+					" " + String.valueOf(Archive.getDifferents()) + " " + String.valueOf(Archive.getSequences()) + "\n";
+		}
+		
 		Archive.writeFile(line);
 		
 		System.exit(0);
